f5400f006791501fb508eae353be14fd0c5f33c0
xmynscnq <xmynscnq@gmail.com>
2025-06-22 11:44:36 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -39,10 +39,10 @@ const API_SITES = {
         api: 'https://tyyszy.com/api.php/provide/vod',
         name: '天涯资源',
     },
-    xiaomaomi: {
-        api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
-        name: '小猫咪资源',
-    },
+    // xiaomaomi: {
+    //     api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
+    //     name: '小猫咪资源',
+    // },
     ffzy: {
         api: 'http://ffzy5.tv/api.php/provide/vod',
         name: '非凡影视',
@@ -123,10 +123,11 @@ const API_SITES = {
     // 这些影片和背后的产业已经使数百万男女“下海”，出卖自己的身体，甚至以此为生计。
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
-    apilsbzy1: {
-        api: 'https://apilsbzy1.com/api.php/provide/vod/at/json',
-        name: '老色'
-        adult: true
+    // ckzy: {
+    //     api: 'https://www.ckzy1.com',
+    //     name: 'CK资源',
+    //     adult: true
+    // },
     // jkun: {
     //     api: 'https://jkunzyapi.com',
     //     name: 'jkun资源',
@@ -145,6 +146,11 @@ const API_SITES = {
     // r155: {
     //     api: 'https://155api.com',
     //     name: '155资源',
+    //     adult: true
+    // },
+    // lsb: {
+    //     api: 'https://apilsbzy1.com',
+    //     name: 'lsb资源',
     //     adult: true
     // },
     // huangcang: {
